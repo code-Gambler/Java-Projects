044ecbf03622b94f65ce5f30eb2891087e67ffc3
code-Gambler <spillay6@myseneca.ca>
2024-04-14 02:45:49 -0400

    removed unwanted files

--- Workshop4&5/module-info.java
+++
@@ -1,8 +0,0 @@
-module inventoryManagementSystem {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	opens controller to javafx.fxml, javafx.graphics;
-	opens model to javafx.base;
-}
